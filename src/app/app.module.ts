488df3f0549348aea3c6f8fb36d6a4b94eef6da8
Sri Sai Vidhya S <103099644+srisaividhya@users.noreply.github.com>
2022-05-22 17:11:37 +0530

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GenreComponent } from './genre/genre.component';
+import { HomeComponent } from './home/home.component';
+import { AuthorsComponent } from './authors/authors.component';
+/*  firebase
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
+
+import { FirebaseService } from './firebase.service'; */ 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GenreComponent,
+    HomeComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    //AngularFireModule.initializeApp(environment.firebase, 'my-book-app'),
+    //AngularFireDatabaseModule
+  ],
+  //providers: [FirebaseService],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
